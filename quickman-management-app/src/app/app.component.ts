b68360cc772d8055695d16a964cbcf7a59148222
Doyka423 <David_Kue@web.de>
2023-11-26 21:38:46 +0100

    Quickman-App-Upload

---
+++ quickman-management-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent]
+})
+export class AppComponent {
+  title = 'quickman-management-app';
+}
